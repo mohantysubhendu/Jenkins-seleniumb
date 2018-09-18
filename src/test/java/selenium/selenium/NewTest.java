5d8060a9dc0bf503e1330f0a139af3d578d8ce88
mohantysubhendu <38078924+mohantysubhendu@users.noreply.github.com>
2018-09-18 17:20:44 +0530

    Update NewTest.java

--- src/test/java/selenium/selenium/NewTest.java
+++ src/test/java/selenium/selenium/NewTest.java
@@ -37,7 +37,8 @@ public class NewTest {
 	
 	  @BeforeClass
 	  public void beforeClass() {
-		 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		// System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		  System.setProperty("webdriver.chrome.driver","/Users/Shared/Jenkins/Home/chromedriver");
 		 driver = new ChromeDriver();
 	  }
 
@@ -108,7 +109,7 @@ public class NewTest {
 	  @Test
 	  public void test_Login() throws InterruptedException, FileNotFoundException, IOException
 	  {
-			System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+			//System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 			Thread.sleep(2000);
 			String baseUrl="https://admin-dev.salusconnect.io";
 			driver.get(baseUrl);
